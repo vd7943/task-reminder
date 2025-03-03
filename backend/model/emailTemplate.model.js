6cdde088b0d387100836e3689639d9dd69f2b00e
VISHESH DALMIA <71868088+vd7943@users.noreply.github.com>
2025-03-03 18:06:48 +0530

    Update emailTemplate.model.js

--- backend/model/emailTemplate.model.js
+++ backend/model/emailTemplate.model.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const emailTemplateSchema = new mongoose.Schema({
-  userType: { type: String, required: true },
+  planName: { type: String, required: true, unique: true },
   subject: { type: String, required: true },
   body: { type: String, required: true },
   taskLink: { type: String, required: true },
