9c8edb616ab1bca10d2079b7a4120abbfae4903d
VISHESH DALMIA <71868088+vd7943@users.noreply.github.com>
2025-03-03 18:57:09 +0530

    Update emailTemplate.controller.js

--- backend/controller/emailTemplate.controller.js
+++ backend/controller/emailTemplate.controller.js
@@ -2,21 +2,26 @@ import EmailTemplate from "../model/emailTemplate.model.js";
 
 export const setEmailTemplate = async (req, res) => {
   try {
-    const { planName, subject, body, taskLink } = req.body;
-    let template = await EmailTemplate.findOne({ planName });
+    const { planName, createdBy, subject, body, taskLink } = req.body;
 
-    if (template) {
-      template.subject = subject;
-      template.body = body;
-      template.taskLink = taskLink;
-    } else {
-      template = new EmailTemplate({
-        planName,
-        subject,
-        body,
-        taskLink,
+    const existingTemplate = await EmailTemplate.findOne({
+      planName,
+      createdBy,
+    });
+
+    if (existingTemplate) {
+      return res.status(400).json({
+        message: "An email template already exists for this plan.",
       });
     }
+
+    const template = new EmailTemplate({
+      planName,
+      createdBy,
+      subject,
+      body,
+      taskLink,
+    });
 
     await template.save();
     res.status(200).json({ message: "Template saved successfully" });
