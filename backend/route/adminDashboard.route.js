13efa78df5bf8e94fadbc7e2102dd8034e33b2d1
VISHESH DALMIA <71868088+vd7943@users.noreply.github.com>
2025-03-06 11:17:21 +0530

    Update adminDashboard.route.js

--- backend/route/adminDashboard.route.js
+++ backend/route/adminDashboard.route.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { getDashboardStats } from "../controller/adminDashboard.controller.js";
+import {
+  getDashboardStats,
+  toggleUserActivation,
+} from "../controller/adminDashboard.controller.js";
 
 const router = express.Router();
 
 router.get("/get-user-data", getDashboardStats);
+router.put("/toggle-user/:userId", toggleUserActivation);
 
 export default router;
