ac54a98a56683ba07f757af6b980de4bf66081a5
VISHESH DALMIA <71868088+vd7943@users.noreply.github.com>
2025-03-06 11:14:33 +0530

    Update planNotificationCron.js

--- backend/automation/planNotificationCron.js
+++ backend/automation/planNotificationCron.js
@@ -19,7 +19,7 @@ export const planNotificationCron = () => {
         if (plan.userRole === "Admin") continue;
 
         const user = await User.findById(plan.userId);
-        if (!user || !user.email) continue;
+         if (!user || !user.email || user.isDeactivated) continue;
 
         for (const task of plan.tasks) {
           const matchingSchedules = task.schedule.filter(
