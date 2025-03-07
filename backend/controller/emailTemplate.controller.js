38829e122b89ef9a0580b64985cdca20b5b25071
VISHESH DALMIA <71868088+vd7943@users.noreply.github.com>
2025-03-07 15:28:16 +0530

    Update emailTemplate.controller.js

--- backend/controller/emailTemplate.controller.js
+++ backend/controller/emailTemplate.controller.js
@@ -4,17 +4,6 @@ export const setEmailTemplate = async (req, res) => {
   try {
     const { planName, createdBy, subject, body } = req.body;
 
-    const existingTemplate = await EmailTemplate.findOne({
-      planName,
-      createdBy,
-    });
-
-    if (existingTemplate) {
-      return res.status(400).json({
-        message: "An email template already exists for this plan.",
-      });
-    }
-
     const template = new EmailTemplate({
       planName,
       createdBy,
@@ -26,5 +15,47 @@ export const setEmailTemplate = async (req, res) => {
     res.status(200).json({ message: "Template saved successfully" });
   } catch (error) {
     res.status(500).json({ message: "Error saving template", error });
+  }
+};
+
+export const getEmailTemplatesByCreatedBy = async (req, res) => {
+  try {
+    const { createdBy } = req.params; // "Custom" or "Admin"
+    const templates = await EmailTemplate.find({ createdBy });
+
+    const groupedTemplates = templates.reduce((acc, template) => {
+      if (!acc[template.planName]) {
+        acc[template.planName] = [];
+      }
+      acc[template.planName].push(template);
+      return acc;
+    }, {});
+
+    res.status(200).json({ templates: groupedTemplates });
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching templates", error });
+  }
+};
+
+export const updateEmailTemplate = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { subject, body } = req.body;
+
+    const updatedTemplate = await EmailTemplate.findByIdAndUpdate(
+      id,
+      { subject, body },
+      { new: true }
+    );
+
+    if (!updatedTemplate) {
+      return res.status(404).json({ message: "Template not found" });
+    }
+
+    res
+      .status(200)
+      .json({ message: "Template updated successfully", updatedTemplate });
+  } catch (error) {
+    res.status(500).json({ message: "Error updating template", error });
   }
 };
