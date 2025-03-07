d30f318567032a2936ed81f89ede1b0697cfa157
VISHESH DALMIA <71868088+vd7943@users.noreply.github.com>
2025-03-07 15:28:57 +0530

    Update emailTemplate.route.js

--- backend/route/emailTemplate.route.js
+++ backend/route/emailTemplate.route.js
@@ -1,9 +1,13 @@
 import express from "express";
 import {
+  getEmailTemplatesByCreatedBy,
   setEmailTemplate,
+  updateEmailTemplate,
 } from "../controller/emailTemplate.controller.js";
 const router = express.Router();
 
-router.post("/set-template", setEmailTemplate);
+router.post("/template/set-template", setEmailTemplate);
+router.get("/templates/:createdBy", getEmailTemplatesByCreatedBy);
+router.put("/update-template/:id", updateEmailTemplate);
 
 export default router;
