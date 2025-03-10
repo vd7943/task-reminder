790248c9db2ca02368a07aa9d7e0e39ab7468860
VISHESH DALMIA <71868088+vd7943@users.noreply.github.com>
2025-03-10 17:19:58 +0530

    Update coinRule.model.js

--- backend/model/coinRule.model.js
+++ backend/model/coinRule.model.js
@@ -4,6 +4,7 @@ const coinRuleSchema = new mongoose.Schema({
   minDuration: { type: Number, required: true },
   coins: { type: Number, required: true },
   freeSubsCoins: { type: Number, required: false },
+  planRestartCoins: { type: Number, required: false },
 });
 
 const CoinRule = mongoose.model("CoinRule", coinRuleSchema);
