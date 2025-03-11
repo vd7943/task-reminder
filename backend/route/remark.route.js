fb493bf8d8107a0c627eda9bee184a0f6b72ae9f
VISHESH DALMIA <71868088+vd7943@users.noreply.github.com>
2025-03-11 21:24:12 +0530

    Update remark.route.js

--- backend/route/remark.route.js
+++ backend/route/remark.route.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { addRemark, getRemark } from "../controller/remark.controller.js";
+import {
+  addRemark,
+  getPlanRemarks,
+  getRemark,
+} from "../controller/remark.controller.js";
 
 const router = express.Router();
 
 router.post("/set-remark", addRemark);
 router.get("/:id", getRemark);
+router.get("/get-remark/:taskId", getPlanRemarks);
 
 export default router;
