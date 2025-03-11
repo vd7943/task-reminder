2c3fcf60167bf24f80c10285e9faad4b9f19ae2d
VISHESH DALMIA <71868088+vd7943@users.noreply.github.com>
2025-03-11 21:23:13 +0530

    Update plan.model.js

--- backend/model/plan.model.js
+++ backend/model/plan.model.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
 
 const TaskSchema = new mongoose.Schema({
+  _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
   taskName: { type: String, required: true },
   taskDescription: String,
   taskLink: String,
   srNo: Number,
+  days: { type: String, required: true },
   schedule: [
     {
       date: { type: String, required: true },
@@ -23,6 +25,7 @@ const planSchema = new mongoose.Schema({
   planName: { type: String, required: true },
   planStart: String,
   tasks: [TaskSchema],
+  status: { type: String, enum: ["Active", "Paused"], default: "Paused" },
   createdAt: { type: Date, default: Date.now },
 });
 
