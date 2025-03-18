16a5198963730f48483161c3a1f87d31e3c40ca9
VISHESH DALMIA <71868088+vd7943@users.noreply.github.com>
2025-03-18 17:35:59 +0530

    Update plan.route.js

--- backend/route/plan.route.js
+++ backend/route/plan.route.js
@@ -28,7 +28,7 @@ router.get("/:id", getPlanById);
 router.get("/coins-earned/:id", getCoinsEarnedForPlan);
 router.put("/update-plan-start/:id", updatePlanStart);
 router.put("/update-task/:planId/:taskId", updateTask);
-router.put("/update-plan-status/:id", updatePlanStatus);
+router.put("/update-plan-status/:id/:userId", updatePlanStatus);
 router.put("/restart-plan", restartPlan);
 
 export default router;
