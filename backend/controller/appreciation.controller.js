9da64d55572ccc241876ae83d2e848d2c4d0e151
VISHESH DALMIA <71868088+vd7943@users.noreply.github.com>
2025-03-24 17:40:26 +0530

    Update appreciation.controller.js

--- backend/controller/appreciation.controller.js
+++ backend/controller/appreciation.controller.js
@@ -2,10 +2,12 @@ import User from "../model/user.model.js";
 
 export const getTopAppreciations = async (req, res) => {
   try {
-    const topUsers = await User.find().sort({ coins: -1 }).limit(3);
+    const topUsers = await User.find({ role: "User" })
+      .sort({ coins: -1 })
+      .limit(3);
 
     if (!topUsers || topUsers.length === 0) {
-      console.log("❌ No users found in database.");
+      console.log("No users found in database.");
       return res
         .status(404)
         .json({ success: false, message: "No users found" });
@@ -13,7 +15,7 @@ export const getTopAppreciations = async (req, res) => {
 
     res.status(200).json({ success: true, topUsers });
   } catch (error) {
-    console.error("❌ Server error:", error);
+    console.error("Server error:", error);
     res
       .status(500)
       .json({ success: false, message: "Server error", error: error.message });
