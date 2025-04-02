bcf230f652b15a29ec04c61642cba3a9f3b7cb67
VISHESH DALMIA <71868088+vd7943@users.noreply.github.com>
2025-04-02 23:15:40 +0530

    Update plan.route.js

--- backend/route/plan.route.js
+++ backend/route/plan.route.js
@@ -8,11 +8,10 @@ import {
   getPlans,
   getTodayPlans,
   optForPlan,
-  updatePlanStart,
   updateTask,
   updatePlanStatus,
   getCoinsEarnedForPlan,
-  restartPlan,
+  deletePlan,
 } from "../controller/plan.controller.js";
 
 const router = express.Router();
@@ -26,9 +25,8 @@ router.post("/milestones", addMilestone);
 router.get("/milestones/:userId", getMilestones);
 router.get("/:id", getPlanById);
 router.get("/coins-earned/:id", getCoinsEarnedForPlan);
-router.put("/update-plan-start/:id", updatePlanStart);
 router.put("/update-task/:planId/:taskId", updateTask);
 router.put("/update-plan-status/:id/:userId", updatePlanStatus);
-router.put("/restart-plan", restartPlan);
+router.delete("/delete/:userId/:planId", deletePlan);
 
 export default router;
