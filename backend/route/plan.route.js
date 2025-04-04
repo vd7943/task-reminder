18c9432efa08f90885d362f8981dc49edc786938
VISHESH DALMIA <71868088+vd7943@users.noreply.github.com>
2025-04-04 18:50:31 +0530

    Update plan.route.js

--- backend/route/plan.route.js
+++ backend/route/plan.route.js
@@ -1,9 +1,7 @@
 import express from "express";
 import {
-  addMilestone,
   addNewPlan,
   getAllPlans,
-  getMilestones,
   getPlanById,
   getPlans,
   getTodayPlans,
@@ -21,8 +19,6 @@ router.post("/opt-plan", optForPlan);
 router.get("/get-plan/:role/:userType", getAllPlans);
 router.get("/get-user-plan/:id", getPlans);
 router.get("/get-today-plan/:id", getTodayPlans);
-router.post("/milestones", addMilestone);
-router.get("/milestones/:userId", getMilestones);
 router.get("/:id", getPlanById);
 router.get("/coins-earned/:id", getCoinsEarnedForPlan);
 router.put("/update-task/:planId/:taskId", updateTask);
